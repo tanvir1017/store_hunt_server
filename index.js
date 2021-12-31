9b1ff99f04250df2817e8fe1bae450e57e002182
Tanvir Hossain <tanvir.hossaindev1@gmail.com>
2021-12-31 16:32:02 +0600

    helloagain

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const admin = require("firebase-admin");
 const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const { MongoClient } = require("mongodb");
 
 // MIDDLEWARE
+
+const serviceAccount = require("./store-hunt-firebase-adminsdk.json");
+// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
 app.use(cors());
 app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.14uaf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -16,12 +25,26 @@ const client = new MongoClient(uri, {
 });
 console.log(uri);
 
+async function verifyToken(req, res, nex) {
+  if (req.headers?.authorization?.startsWith("Bearer ")) {
+    const token = req.headers.authorization.split(" ")[1];
+
+    try {
+      const decodedUser = await admin.auth().verifyIdToken(token);
+      req.decodedEmail = decodedUser.email;
+    } catch {}
+  }
+
+  nex();
+}
+
 async function run() {
   try {
     await client.connect();
     const databse = client.db("Store-hunt");
     const productCollection = databse.collection("product");
     const ordersCollection = databse.collection("order");
+    const usersCollection = databse.collection("users");
 
     //   app.get for all products
     app.get("/product", async (req, res) => {
@@ -71,6 +94,16 @@ async function run() {
       const result = await ordersCollection.deleteOne(query);
       res.json(result);
     });
+    // UPDATE STATUS
+    app.put("/manageOrders", async (req, res) => {
+      const id = req.body._id;
+      const checked = req.body.checked;
+      const query = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const data = { $set: { pending: checked } };
+      const result = await ordersCollection.updateOne(query, data, options);
+      res.json(result);
+    });
     // ADD PRODUCT VY AXIOS
     app.post("/product", async (req, res) => {
       const product = req.body;
@@ -91,6 +124,62 @@ async function run() {
       const query = { _id: ObjectId(id) };
       const result = await productCollection.deleteOne(query);
       res.json(result);
+    });
+
+    // save data to the database
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.json(result);
+      console.log(result);
+    });
+
+    // GET ADMIN BY ROLE
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const user = await usersCollection.findOne(filter);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+
+    // Update User data to the database
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+      console.log(result);
+    });
+
+    // SET ADMIN ROLE
+    app.put("/users/admin", verifyToken, async (req, res) => {
+      const user = req.body;
+      const requester = req.decodedEmail;
+      if (requester) {
+        const requesterIdenty = await usersCollection.findOne({
+          email: requester,
+        });
+        if (requesterIdenty.role === "admin") {
+          const filter = { email: user.email };
+          const updateDoc = { $set: { role: "admin" } };
+          const result = await usersCollection.updateOne(filter, updateDoc);
+          res.json(result);
+        }
+      } else {
+        res
+          .status(403)
+          .json({ message: "you do not have any permission to access" });
+      }
     });
   } finally {
     // await client.close();
